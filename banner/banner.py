47d4f9bfc444eb4244a0134a5e1296b251fce42a
MUKESHKUMAR2001 <mukeshkumarckarak@gmail.com>
2021-04-21 23:45:00 +0530

    Banner of tool

---
+++ banner/banner.py
@@ -0,0 +1,41 @@
+#!/bin/bash
+from __future__ import print_function
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
+from terminaltables import SingleTable
+print(Fore.BLUE+Back.YELLOW+"Hi My name is Mukesh Kumar "+ Fore.YELLOW+ Back.BLUE+"Get this tool from https://github.com/hakxcore/Timepass            ") 
+table_instance = SingleTable([["""                                                                     
+                         .-==++*++*+*+=:                    
+                     -+*%@@@@@@@@@@@@@@@@%*:                
+                   =%@@@@@@@@@@@@@@@@@@@@@@@%+.             
+                  +@@@@@@@@@@@@@@@@@@@@@@@@@@@@=            
+                 #@@@@@@@@@@@@@@#*%@@@@@@@@@@@@@%=          
+                *@@@@@@@@@@@@*=.    :::=*@@@@@@@@@%         
+               .@@@@@@@@@@@@@:            ::-=*@@@@         
+               +@@@@@@@@@@@@*.         .:      -@%:         
+               *=-=%@@@@@@+.         .---=:..:. #+          
+              +-  .:+%@@@+     .+#%%%@%**+.:#*-.==          
+              =:  -.  *@@     :-*:::-:    .:.+@%*           
+              -%.  -   ..     .-+:-=*#  : -..:=#@=          
+              -...-:              ::..    *+=+= *:          
+               -  .                       *=..+*.           
+                %-:.             ::.-.    :.-  -            
+                ++.:        :  ::   :.::.  +.=  :           
+                -%-:       - -- :-=:...   ::.: :.           
+                 #-        - -=%%+==+=-=+=-. -::            
+                 -:         -: +*%#%%*+=+@#-::.             
+                 -:.        .-  ::-:--+*++-::               
+                .-  :.       --.   .....:=-.                
+                =    :.                 .=                  
+              :=               :         :                  
+             .--                ::..  :-:                   
+         .::. +                    .-:=-.                   
+         :    :                      -  ::                  
+                       =                                         
+                                          Author: Hakxcore                            
+                                          Email:  anonymous_mails_box@protonmail.ch       
+                                          Github: https://github.com/hakxcore             
+"""]], '(hakxcore)')
+print(Fore.BLACK+Back.WHITE+(table_instance.table))
+print()
